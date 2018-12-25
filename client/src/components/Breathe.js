f3fdb25dd42619c3f2183ba77f1503530937c711
konradgnat <konradmgnat@gmail.com>
2018-12-25 16:06:53 -0600

    add breathe page

--- client/src/components/Breathe.js
+++ client/src/components/Breathe.js
@@ -6,6 +6,22 @@ class Breathe extends Component {
     return (
       <div className="container">
         breathe app
+        <div className="breathe__inner">
+          <div className="breathe__time_view">
+            <input type="number" name="time" />
+          </div>
+          <div className="breathe__time_set">
+            <input type="range" interval="100"/>
+          </div>
+          <div className="breathe__start">
+            <button><i className="small material-icons">spa</i>Start</button>
+          </div>
+          <div className="breathe__center">
+            <div className="breathe">
+              <div className="breathe__counter"></div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
